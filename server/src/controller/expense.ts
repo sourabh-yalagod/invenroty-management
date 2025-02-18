ff021ddb38e389dbf989c5cd99a3fc09443894c2
sourabh-yalagod <sourabhofficial99804@gmail.com>
2025-02-18 18:46:32 +0530

    type Error

--- server/src/controller/expense.ts
+++ server/src/controller/expense.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Products } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 const prisma = new PrismaClient();
 
