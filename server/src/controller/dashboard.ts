54865671b2ce2f3c8e901c71e25db2b3272662cb
sourabh-yalagod <sourabhofficial99804@gmail.com>
2025-02-19 00:12:04 +0530

    client build Error

--- server/src/controller/dashboard.ts
+++ server/src/controller/dashboard.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { ExpenseByCategory, PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 const prisma = new PrismaClient();
 export const getDashboardMatrics = async (
